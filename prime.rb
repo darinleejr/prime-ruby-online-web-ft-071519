3d1473dfdfe2a8cef127456dcb47bc948c743472
Darin Webster <darinleenavy@gmail.com>
2019-08-22 16:54:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
 def prime?()
+  
+end
+
+
+def prime?(integer)
+  test_divisors = (2..(integer - 1)).to_a	  test_divisors = (2..(integer - 1)).to_a
+  if test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1 	  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true
